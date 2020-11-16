68045bc4289ee94b5932c7bdc3f4b33205e0404a
leena0203 <leenasarode02@gmail.com>
2020-11-17 03:14:28 +0530

    Added: calculate wage in month for max hours condition

--- employee.js
+++ employee.js
@@ -14,6 +14,7 @@ const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_WORKING_HRS = 160;
 
 function getWorkingHrs(empCheck) {
     switch (empCheck) {
@@ -33,3 +34,14 @@ for (var i = 0; i < NUM_OF_WORKING_DAYS; i++) {
 }
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Total hrs: " + empHrs + " Emp wage is : " + empWage);
+
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while (totalEmpHrs < MAX_WORKING_HRS && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHrs(empCheck);
+    totalEmpHrs += empHrs;
+}
+let employeeWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total hrs : " + totalEmpHrs + " Total Employee Wage : " + employeeWage);
