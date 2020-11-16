a7b80c36fb3f024fae6a21077515926adebeef91
leena0203 <leenasarode02@gmail.com>
2020-11-17 02:52:48 +0530

    Added: daily wage of employee

--- employee.js
+++ employee.js
@@ -1,4 +1,5 @@
-const IS_ABSENT = 0
+{
+    const IS_ABSENT = 0
 let empCheck = Math.floor(Math.random() * 10) % 2;
 if (empCheck == IS_ABSENT) {
     console.log("Employee is Absent");
@@ -6,3 +7,26 @@ if (empCheck == IS_ABSENT) {
 } else {
     console.log("Employee is Present");
 } 
+}
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HRS = 4;
+const FULL_TIME_HRS = 8;
+const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
+
+let empCheck = Math.floor(Math.random() * 10) % 3;
+let empHrs = 0;
+switch (empCheck) {
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HRS;
+        break;
+    case IS_FULL_TIME:
+        empHrs = FULL_TIME_HRS;
+        break;
+    default:
+        empHrs = 0;
+}
+
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Daily wage : " + empWage);
